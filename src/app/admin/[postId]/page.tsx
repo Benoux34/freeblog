7bc62b1ca03b39604c9a035e033bed6491e930d7
Benoit <benoitantunes34130@gmail.com>
2025-02-11 15:34:47 +0100

    fix: vercel deploy

--- src/app/admin/[postId]/page.tsx
+++ src/app/admin/[postId]/page.tsx
@@ -2,12 +2,6 @@ import { PostForm } from "@/components/Posts/CreatePost/Form/PostForm";
 import prisma from "@/lib/db";
 import { redirect } from "next/navigation";
 
-type Props = {
-  params: {
-    postId: string;
-  };
-};
-
 async function getPost(postId: string) {
   const post = await prisma.post.findUnique({
     where: {
@@ -21,13 +15,14 @@ async function getPost(postId: string) {
   return post;
 }
 
-export default async function Post({ params }: Props) {
-  const resolvedParams = await params;
-  if (!resolvedParams.postId) redirect("/");
+export default async function Post({
+  params: { postId },
+}: {
+  params: { postId: string };
+}) {
+  if (!postId) redirect("/");
 
-  const post_id = resolvedParams?.postId;
-
-  const post = await getPost(post_id);
+  const post = await getPost(postId);
   console.log(post);
 
   return (
@@ -37,7 +32,7 @@ export default async function Post({ params }: Props) {
       </div>
 
       <div className="w-1/2">
-        <PostForm postId={post_id} initialData={post} />
+        <PostForm postId={postId} initialData={post} />
       </div>
     </main>
   );
